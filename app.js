f4f05d82cb64a5e07b5235846fbf954eab8a1044
unknown <berkelmas96@gmail.com>
2019-04-06 17:38:01 +0300

    custom namespace içerisine room eklemesi yapılarak; haberleşme sağlandı.

--- app.js
+++ app.js
@@ -12,10 +12,11 @@ const io = socketio.listen(server);
 const nsp = io.of('/namespace');
 
 nsp.on('connection', (socket) => {
-    console.log('Bağlantı başarılı...');
+    socket.join('anaOda') // Ana Oda Diye bir oda varsa ona katılınacak; yoksa böyle bir oda oluşturulacak ve katılınacak.
+    console.log('anaOda ya katılındı...')
 
     socket.on('deneme', () => {
-        socket.emit('deneme', {veri : 'deneme'})
+        socket.to('anaOda').emit('deneme', {veri : 'deneme'})
     })
 
     socket.on('disconnect', () => {
